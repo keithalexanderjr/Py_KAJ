6098bb6c2de3ea64adfa3e5bc1bbc877e94bf4cd
Keith Alexander Jr <99139089+keithalexanderjr@users.noreply.github.com>
2023-06-08 19:02:11 -0500

    Divisibility Calculator Push

---
+++ divisible_kaj/main.py
@@ -0,0 +1,14 @@
+def divisible_by_ten(num):
+  if (num % 10 == 0):
+    return True
+  else:
+    return False
+
+# Test case 1:
+
+print(divisible_by_ten(20))
+# Result: print True
+
+# Test case 2:
+print(divisible_by_ten(25))
+# Result: print False
