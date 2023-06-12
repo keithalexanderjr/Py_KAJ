10c1a8843ae733921b252bb80dc713ca2ba860a0
Keith Alexander Jr <99139089+keithalexanderjr@users.noreply.github.com>
2023-06-11 22:34:18 -0500

    Summation Calc Code Challenge

---
+++ sum_calc_kaj/main.py
@@ -0,0 +1,13 @@
+def not_sum_to_ten(num1, num2):
+  if (num1 + num2 != 10):
+    return True
+  else:
+    return False
+# Uncomment these function calls to test not_sum_to_ten function:
+
+print(not_sum_to_ten(9, -1))
+# Expected Result: print True
+print(not_sum_to_ten(9, 1))
+# Expected Result: print False
+print(not_sum_to_ten(5,5))
+# Expected Result: print False
