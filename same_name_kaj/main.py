3cd0b40912daa8f6754e61fd49f995f968bdb035
Keith Alexander Jr <99139089+keithalexanderjr@users.noreply.github.com>
2023-06-13 20:24:14 -0500

    Name Comparison Function Challenge

---
+++ same_name_kaj/main.py
@@ -0,0 +1,11 @@
+def same_name(your_name, my_name):
+  if (your_name == my_name):
+    return True
+  else:
+    return False
+# same_name function:
+print(same_name("Colby", "Colby"))
+# Result: print True
+
+print(same_name("Tina", "Amber"))
+# Result: print False
